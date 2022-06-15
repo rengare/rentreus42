b42a840ce8c2f899e1ccf7438929b19575780f6f
ggorecki <gorecki.gracjan@gmail.com>
2022-06-15 14:39:40 +0200

    changes to the layout

--- main.py
+++ main.py
@@ -45,19 +45,19 @@ keyboard.keymap = [
         KC.Q, KC.W, KC.E,  KC.R,  KC.T,                      XXX,        KC.Y, KC.U, KC.I,    KC.O,   KC.P,     
         KC.A, KC.S, KC.D,  KC.F,  KC.G,                      XXX,        KC.H, KC.J, KC.K,    KC.L,   KC.SCLN,  
         KC.Z, KC.X, KC.C,  KC.V,  KC.B,                      ALT,        KC.N, KC.M, KC.COMM, KC.DOT, KC.SLSH,
-        ESC,  TAB,  SUPER, SHIFT, SPACE,                     CTRL,       BACK, L1,   RALT,    KC.QUOTE,  ENTER,  
+        ESC,  TAB,  SUPER, SHIFT, SPACE,                     CTRL,       BACK, RALT,   L1,    KC.QUOTE,  ENTER,  
     ],
     [
         KC.EXCLAIM, KC.AT, KC.LPRN, KC.RPRN, KC.PIPE,        XXX,        KC.PGUP,  KC.N7, KC.N8,   KC.N9,   KC.ASTERISK,     
         KC.HASH, KC.DLR, KC.LBRC, KC.RBRC   ,KC.ZKHK,        XXX,        KC.PGDN,  KC.N4, KC.N5,   KC.N6,   KC.MINUS,     
         KC.PERC, KC.AMPR, ___, ___, ___,                     TRANS,      ___,      KC.N1, KC.N2,   KC.N3,   KC.BSLS,     
-        TRANS, L2, TRANS, TRANS, TRANS,                      TRANS,      L2,       L0,    KC.DOT,  KC.N0,   KC.EQUAL, 
+        TRANS, L2, TRANS, TRANS, TRANS,                      TRANS,      L2,       KC.DOT,  L0  ,  KC.N0,   KC.EQUAL, 
     ],
     [
         KC.PSCR, KC.HOME, KC.UP, KC.END, KC.PGUP,           XXX,        KC.PGUP,   KC.F7, KC.F8,   KC.F9,  KC.F10,       
         DEL, KC.LEFT, KC.DOWN, KC.RIGHT,KC.PGDN,            XXX,        KC.PGDN,   KC.F4, KC.F5,   KC.F6,  KC.F11,    
         ___, ___, ___, ___, ___,                            TRANS,      KC.AMPR,   KC.F1, KC.F2,   KC.F3,  KC.F12,   
-        TRANS, L0, TRANS, TRANS, TRANS,                     TRANS,      L1,        L0,    ___, ___, ENTER,  
+        TRANS, L0, TRANS, TRANS, TRANS,                     TRANS,      L1,          L0,     L0 ,   ___,   ENTER,  
     ],
 ]
 
