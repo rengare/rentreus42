f86326a22d40bd97010970fbdeff2fb3c9f98822
ggorecki <gorecki.gracjan@gmail.com>
2022-06-15 08:18:58 +0200

    changes to the keyboard layout

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ ___ = KC.NO
 XXX = KC.NO
 
 # Layers 
-BASE = KC.TO(0)	
+L0 = KC.TO(0)	
 L1 = KC.TT(1)
 L2 = KC.TT(2)
 
@@ -42,25 +42,24 @@ keyboard.debug_enabled = False
 # make keymap
 keyboard.keymap = [
     [
-        KC.Q, KC.W, KC.E,  KC.R,  KC.T,                   XXX,        KC.Y, KC.U, KC.I,    KC.O,   KC.P,     
-        KC.A, KC.S, KC.D,  KC.F,  KC.G,                   XXX,        KC.H, KC.J, KC.K,    KC.L,   KC.SCLN,  
-        KC.Z, KC.X, KC.C,  KC.V,  KC.B,                   ALT,       KC.N, KC.M, KC.COMM, KC.DOT, KC.SLSH,
-        ESC,  TAB,  SHIFT, SUPER, SPACE,                  CTRL,        BACK, RALT, L1, L2,  ENTER,  
+        KC.Q, KC.W, KC.E,  KC.R,  KC.T,                      XXX,        KC.Y, KC.U, KC.I,    KC.O,   KC.P,     
+        KC.A, KC.S, KC.D,  KC.F,  KC.G,                      XXX,        KC.H, KC.J, KC.K,    KC.L,   KC.SCLN,  
+        KC.Z, KC.X, KC.C,  KC.V,  KC.B,                      ALT,        KC.N, KC.M, KC.COMM, KC.DOT, KC.SLSH,
+        ESC,  TAB,  SUPER, SHIFT, SPACE,                     CTRL,       BACK, L1,   RALT,    KC.QUOTE,  ENTER,  
     ],
     [
-        KC.N7, KC.N8, KC.N9, KC.N0, ___,                  XXX,       KC.F7, KC.F8, KC.F9, KC.F10,   DEL,     
-        KC.N4, KC.N5, KC.N6, KC.LBRACKET, KC.RBRACKET,    XXX,       KC.F4, KC.F5, KC.F6, KC.F11,   TRANS,  
-        KC.N1, KC.N2, KC.N3, KC.MINUS,  KC.EQUAL,         TRANS,      KC.F1, KC.F2, KC.F3, KC.F12,  TRANS,
-        TRANS, TRANS, TRANS, TRANS, TRANS,                TRANS,      TRANS, TRANS, BASE, BASE, TRANS, 
-        
+        KC.EXCLAIM, KC.AT, KC.LPRN, KC.RPRN, KC.PIPE,        XXX,        KC.PGUP,  KC.N7, KC.N8,   KC.N9,   KC.ASTERISK,     
+        KC.HASH, KC.DLR, KC.LBRC, KC.RBRC   ,KC.ZKHK,        XXX,        KC.PGDN,  KC.N4, KC.N5,   KC.N6,   KC.MINUS,     
+        KC.PERC, KC.AMPR, ___, ___, ___,                     TRANS,      ___,      KC.N1, KC.N2,   KC.N3,   KC.BSLS,     
+        TRANS, L2, TRANS, TRANS, TRANS,                      TRANS,      L2,       L0,    KC.DOT,  KC.N0,   KC.EQUAL, 
     ],
     [
-        ___, KC.UP, ___, ___,  ___,                       XXX,       ___,  KC.PGUP,  KC.PGDOWN, ___,  ___,
-        KC.LEFT, KC.DOWN, KC.RIGHT, ___,  ___,            XXX,       ___,  KC.HOME,  KC.END, ___,  ___,
-        ___,  ___,  ___, ___,  ___,                       TRANS,      ___,  ___,  ___, ___,  ___,
-        TRANS, TRANS, TRANS, TRANS, TRANS,                TRANS,      TRANS, TRANS, BASE, BASE, TRANS, 
+        KC.PSCR, KC.HOME, KC.UP, KC.END, KC.PGUP,           XXX,        KC.PGUP,   KC.F7, KC.F8,   KC.F9,  KC.F10,       
+        DEL, KC.LEFT, KC.DOWN, KC.RIGHT,KC.PGDN,            XXX,        KC.PGDN,   KC.F4, KC.F5,   KC.F6,  KC.F11,    
+        ___, ___, ___, ___, ___,                            TRANS,      KC.AMPR,   KC.F1, KC.F2,   KC.F3,  KC.F12,   
+        TRANS, L0, TRANS, TRANS, TRANS,                     TRANS,      L1,        L0,    ___, ___, ENTER,  
     ],
-   ]
+]
 
 if __name__ == '__main__':
     keyboard.go()
